9bd3639cf3693799a4feac7f294afc3973cc3e1e
samuelchardy <s.hardy3@lancaster.ac.uk>
2018-04-28 12:03:23 +0100

    2 : Line to point firing angle and line movement via left/ right keys. Balls appear at line end coordinates when up key is pressed.

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -1,7 +1,56 @@
+import java.lang.Math.*;
+
 public class BrickBreaker
 {
-   public static void main(String args[]){
-       GameArena g = new GameArena(300,600);    
+
+    void input(GameArena g, Line arrow, Ball[] balls)
+    {
+        int i = 0;
+        while(true){
+            if(g.rightPressed() == true){
+                if(arrow.getEndX() >= 150){
+                    arrow.setEnd(arrow.getEndX()+1, arrow.getEndY()+1);
+                }else{
+                    arrow.setEnd(arrow.getEndX()+1, arrow.getEndY()-1);
+                }
+            }else if(g.leftPressed() == true){
+                if(arrow.getEndX() <= 150){
+                    arrow.setEnd(arrow.getEndX()-1, arrow.getEndY()+1);
+                }else{
+                    arrow.setEnd(arrow.getEndX()-1, arrow.getEndY()-1);
+                }
+            }else if(g.upPressed() == true){
+                int xDir = Math.abs((int)(arrow.getEndX() - arrow.getStartX()));
+                int yDir = Math.abs((int)(arrow.getEndY() - arrow.getStartY()));
+                if(i < balls.length){
+                    balls[i] = new Ball(arrow.getEndX(), arrow.getEndY(), 3, "RED");
+                    g.addBall(balls[i]);
+                }
+                i++;
+            }
+            arrow.setStart(arrow.getStartX(), arrow.getStartY());
+            g.update();
+        }
+
+    }
+
+
+   public static void main(String args[])
+   {
+       BrickBreaker b = new BrickBreaker();
+
+       GameArena g = new GameArena(300,450);
+       Line arrow = new Line(150,450,150,410,2,"WHITE");
+       Ball[] balls = new Ball[20];
+       //Rectangle[][] bricks = new Rectangle[2][10];
+
+       g.addLine(arrow);
+       //b.initRectangles(g, bricks);
+       g.update();
+
+
+
+       b.input(g, arrow, balls);
    }
 
 
