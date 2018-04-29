5fbaae052b190cf15099c33d6faeebe13d7fb2fa
samuelchardy <s.hardy3@lancaster.ac.uk>
2018-04-29 14:56:16 +0100

    7 : Added Main.java which creates an instance of BrickBreaker.java. Made GameArena components private instance variables for ease of use throughout the BrickBreaker.java class.

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -3,6 +3,12 @@ import java.lang.Math.*;
 public class BrickBreaker
 {
 
+    private GameArena g = new GameArena(300,450);
+    private Line arrow = new Line(150,450,150,410,2,"WHITE");
+    private Projectile[] balls = new Projectile[20];
+    private Rectangle[][] bricks = new Rectangle[2][10];
+    private int i = 0;
+
 
     void collisionDetection(Projectile ball)
     {
@@ -20,7 +26,7 @@ public class BrickBreaker
 
 
 
-    void movement(int i, Projectile[] balls)
+    void movement(int i)
     {
         for(int c=0; c<i; c++){
             Ball b = balls[c].getBall();
@@ -31,68 +37,74 @@ public class BrickBreaker
     }
 
 
-    void input(GameArena g, Line arrow, Projectile[] balls)
+    void input()
     {
-        int i = 0;
         int resistance = 5;
-        while(true){
-            if(g.rightPressed() == true){
-                if(arrow.getEndX() >= 150){
-                    arrow.setEnd(arrow.getEndX()+1, arrow.getEndY()+1);
-                }else{
-                    arrow.setEnd(arrow.getEndX()+1, arrow.getEndY()-1);
-                }
-            }else if(g.leftPressed() == true){
-                if(arrow.getEndX() <= 150){
-                    arrow.setEnd(arrow.getEndX()-1, arrow.getEndY()+1);
-                }else{
-                    arrow.setEnd(arrow.getEndX()-1, arrow.getEndY()-1);
-                }
-            }else if(g.upPressed() == true){
-                if(i < balls.length){
-                    balls[i] = new Projectile(arrow.getEndX(), arrow.getEndY(), 3, "RED",g);
 
-                    balls[i].setxDirection((arrow.getEndX() - arrow.getStartX())/resistance);
-                    balls[i].setyDirection((arrow.getEndY() - arrow.getStartY())/resistance);
-                    //System.out.println("xDir: " + balls[i].getxDirection() + "   yDir: " + balls[i].getyDirection());
-                    i++;
-                }
+        if(g.rightPressed() == true){
+            if(arrow.getEndX() >= 150){
+                arrow.setEnd(arrow.getEndX()+1, arrow.getEndY()+1);
+            }else{
+                arrow.setEnd(arrow.getEndX()+1, arrow.getEndY()-1);
             }
-            arrow.setStart(arrow.getStartX(), arrow.getStartY());
+        }else if(g.leftPressed() == true){
+            if(arrow.getEndX() <= 150){
+                arrow.setEnd(arrow.getEndX()-1, arrow.getEndY()+1);
+            }else{
+                arrow.setEnd(arrow.getEndX()-1, arrow.getEndY()-1);
+            }
+        }else if(g.upPressed() == true){
+            if(i < balls.length){
+                balls[i] = new Projectile(arrow.getEndX(), arrow.getEndY(), 3, "RED",g);
 
-            movement(i, balls);
+                balls[i].setxDirection((arrow.getEndX() - arrow.getStartX())/resistance);
+                balls[i].setyDirection((arrow.getEndY() - arrow.getStartY())/resistance);
+                //System.out.println("xDir: " + balls[i].getxDirection() + "   yDir: " + balls[i].getyDirection());
+                i++;
+            }
+        }
+        arrow.setStart(arrow.getStartX(), arrow.getStartY());
+
+        movement(i);
+    }
+
+
+    void menu()
+    {
+        while(true){
+            input();
             g.update();
         }
 
     }
 
-    void initRectangles(GameArena g, Rectangle[][] bricks)
+    void initRectangles()
     {
-        for(int i=0; i<bricks.length; i++){
-            for(int c=0; c<bricks[i].length; c++){
+        for(int v=0; v<bricks.length; v++){
+            for(int c=0; c<bricks[v].length; c++){
                 int xPos = (c*29)+20;
-                int yPos = (i*25)+15;
-                bricks[i][c] = new Rectangle(xPos, yPos, 27, 20, "WHITE");
-                g.addRectangle(bricks[i][c]);
+                int yPos = (v*25)+15;
+                bricks[v][c] = new Rectangle(xPos, yPos, 27, 20, "WHITE");
+                g.addRectangle(bricks[v][c]);
             }
         }
     }
 
 
+    public BrickBreaker()
+    {
+
+        g.addLine(arrow);
+        initRectangles();
+        g.update();
+
+        menu();
+    }
+
+
    public static void main(String args[])
    {
-       BrickBreaker b = new BrickBreaker();
 
-       GameArena g = new GameArena(300,450);
-       Line arrow = new Line(150,450,150,410,2,"WHITE");
-       Projectile[] balls = new Projectile[20];
-       Rectangle[][] bricks = new Rectangle[2][10];
-
-       g.addLine(arrow);
-       b.initRectangles(g, bricks);
-       g.update();
-
-       b.input(g, arrow, balls);
    }
 
 
